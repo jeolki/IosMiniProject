c51651bd56a35cd3679f1da1c5792bda217453a1
hongjeonggi <hongjeonggi35@gmail.com>
2021-11-02 19:15:10 +0900

    Add Pomodoro Project and images Add Basic UI

---
+++ 01_basic/06_01_Pomodoro/Pomodoro/Pomodoro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pomodoro
+//
+//  Created by Jeonggi Hong on 2021/11/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
