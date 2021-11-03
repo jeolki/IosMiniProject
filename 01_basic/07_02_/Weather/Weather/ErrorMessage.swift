384645a130f95933504b2808308d2c1ff8bf64bc
hongjeonggi <hongjeonggi35@gmail.com>
2021-11-03 17:23:35 +0900

    Add success operation view and alert error msg Add errorMessage struct

---
+++ 01_basic/07_02_/Weather/Weather/ErrorMessage.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorMessage.swift
+//  Weather
+//
+//  Created by Jeonggi Hong on 2021/11/03.
+//
+
+import Foundation
+
+struct ErrorMessage: Codable{
+    let message: String
+}
--- 01_basic/07_02_/Weather/Weather/ViewController.swift
+++ 01_basic/07_02_/Weather/Weather/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var maxTempLabel: UILabel!
     @IBOutlet weak var minTempLabel: UILabel!
     
+    @IBOutlet weak var weatherStackView: UIStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,15 +29,54 @@ class ViewController: UIViewController {
         }
     }
     
+    func showAlert(message: String) {
+        let alert = UIAlertController(title: "에러", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "확인", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
     func getCurrentWeather(cityName: String) {
         guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(cityName)&appid=2e24da29b756a32827d4f8b1a333e43c") else { return }
         let session = URLSession(configuration: .default)
-        session.dataTask(with: url) { data, response, error in
+        session.dataTask(with: url) { [weak self] data, response, error in
+            
+            // 에러와 성공을 분리하기 위해서
+            let successRange = (200..<300)
+            
             guard let data = data, error == nil else { return }
             let decoder = JSONDecoder()
-            let weatherInfomation = try? decoder.decode(WeatherInformation.self, from: data)
-            debugPrint(weatherInfomation)
+            
+            // 성공과 에러발생을 분리 응답코드를 확인
+            if let response = response as? HTTPURLResponse, successRange.contains(response.statusCode) {
+                guard let weatherInfomation = try? decoder.decode(WeatherInformation.self, from: data) else { return }
+                
+                // 네트워크 작업은 별도의 스레드에서 작업되고 완료되더라도 메인스레드로 돌아오지 않기때문에
+                // 메인스레드로 돌아오도록 해줘야합니다.
+                DispatchQueue.main.async {
+                    // 메인스레드 안에서 UI작업을 할 수 있도록
+                    self?.weatherStackView.isHidden = false
+                    self?.configureView(weatherInformation: weatherInfomation)
+                }
+                
+            } else {
+                guard let errorMessage = try? decoder.decode(ErrorMessage.self, from: data) else { return }
+                DispatchQueue.main.async {
+                    self?.showAlert(message: errorMessage.message)
+                }
+            }
+            
         }.resume()
+    }
+    
+    // 화면 설정
+    func configureView(weatherInformation: WeatherInformation) {
+        self.cityNameLabel.text = weatherInformation.name
+        if let weather = weatherInformation.weather.first {
+            self.weatherDescriptionLabel.text = weather.description
+        }
+        self.tempLabel.text = "\(Int(weatherInformation.temp.temp - 273.15))°C"
+        self.minTempLabel.text = "최저: \(Int(weatherInformation.temp.minTemp - 273.15))°C"
+        self.maxTempLabel.text = "최고: \(Int(weatherInformation.temp.maxTemp - 273.15))°C"
     }
     
 }
