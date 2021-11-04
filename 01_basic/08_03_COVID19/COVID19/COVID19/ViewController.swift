33e4b9e5710bfc230fc30ab5a2bc07280819f972
hongjeonggi <hongjeonggi35@gmail.com>
2021-11-04 11:57:51 +0900

    Add API to get covid data

--- 01_basic/08_03_COVID19/COVID19/COVID19/ViewController.swift
+++ 01_basic/08_03_COVID19/COVID19/COVID19/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+
+import Alamofire
 import Charts
 
 class ViewController: UIViewController {
@@ -17,9 +19,43 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.fetchCovidOverview(completionHandler: { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case let .success(result):
+                debugPrint("success \(result)")
+            case let .failure(error):
+                debugPrint("error \(error)")
+            }
+        })
     }
 
+    func fetchCovidOverview(
+        completionHandler: @escaping (Result<CityCovidOverView, Error>) -> Void
+    ) {
+        let url = "https://api.corona-19.kr/korea/country/new/"
+        let param = [
+            "serviceKey": "PLVhSKZrWMqt2NJRsy1pUbcFgoH4kidX3"
+        ]
+        
+        // API 호출
+        AF.request(url, method: .get, parameters: param)
+            .responseData(completionHandler: { response in
+                switch response.result {
+                case let .success(data):
+                    do {
+                        let decoder = JSONDecoder()
+                        let result = try decoder.decode(CityCovidOverView.self, from: data)
+                        completionHandler(.success(result))
+                    } catch {
+                        completionHandler(.failure(error))
+                    }
+                case let .failure(error):
+                    completionHandler(.failure(error))
+                }
+            })
+        
+    }
 
 }
 
