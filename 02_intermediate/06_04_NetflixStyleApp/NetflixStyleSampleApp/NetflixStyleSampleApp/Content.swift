69812e64ae3a597bfe8bdc615fc42841d1c0d0e9
hongjeonggi <hongjeonggi35@gmail.com>
2021-11-12 16:40:02 +0900

    Add basic UI

---
+++ 02_intermediate/06_04_NetflixStyleApp/NetflixStyleSampleApp/NetflixStyleSampleApp/Content.swift
@@ -0,0 +1,32 @@
+//
+//  Content.swift
+//  NetflixStyleSampleApp
+//
+//  Created by Jeonggi Hong on 2021/11/12.
+//
+
+import UIKit
+
+struct Content: Decodable {
+    let sectionType: SectionType
+    let sectionName: String
+    let contentItem: [Item]
+    
+    enum SectionType: String, Decodable {
+        case basic
+        case main
+        case large
+        case rank
+    }
+}
+
+struct Item: Decodable {
+    let description: String
+    let imageName: String
+    
+    var image: UIImage {
+        return UIImage(named: imageName) ?? UIImage()
+    }
+}
+
+
