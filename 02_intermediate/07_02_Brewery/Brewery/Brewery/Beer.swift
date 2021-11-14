f400d9d421eaf55373fb0d69b4c4a188bd31ee66
hongjeonggi <hongjeonggi35@gmail.com>
2021-11-15 01:36:59 +0900

    Add Brewery project Basic UI

---
+++ 02_intermediate/07_02_Brewery/Brewery/Brewery/Beer.swift
@@ -0,0 +1,34 @@
+//
+//  Beer.swift
+//  Brewery
+//
+//  Created by Jeonggi Hong on 2021/11/15.
+//
+
+import Foundation
+
+struct Beer: Decodable {
+    let id: Int?
+    let name, taglineString, description, brewersTips, imageURL: String?
+    let foodParing: [String]?
+    
+    // 태그 문자열 변형
+    var tagLine: String {
+        let tags = taglineString?.components(separatedBy: ". ")
+        let hashtags = tags?.map {
+            "#" + $0.replacingOccurrences(of: " ", with: "")
+                .replacingOccurrences(of: ".", with: "")
+                .replacingOccurrences(of: ",", with: " #")
+        }
+        return hashtags?.joined(separator: " ") ?? "" // #tag #good
+    }
+    
+    // 서버에서 받은 명칭과 변수명칭이 다를때 설정
+    enum CodingKeys: String, CodingKey {
+        case id, name, description
+        case taglineString = "tagline"
+        case imageURL = "image_url"
+        case brewersTips = "brewers_tips"
+        case foodParing = "food_paring"
+    }
+}
