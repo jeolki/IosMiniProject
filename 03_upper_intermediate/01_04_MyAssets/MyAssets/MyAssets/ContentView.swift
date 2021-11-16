e06969822f597073750d8f245e83d57be0d15afe
hongjeonggi <hongjeonggi35@gmail.com>
2021-11-16 20:08:47 +0900

    Add : MyAssets project First commit

---
+++ 03_upper_intermediate/01_04_MyAssets/MyAssets/MyAssets/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyAssets
+//
+//  Created by Jeonggi Hong on 2021/11/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 03_upper_intermediate/01_04_MyAssets/MyAssets/MyAssets/MyAssetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAssetsApp.swift
+//  MyAssets
+//
+//  Created by Jeonggi Hong on 2021/11/16.
+//
+
+import SwiftUI
+
+@main
+struct MyAssetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
