38325a6b7b9b384b29fa30791c8efc77efe29736
hongjeonggi <hongjeonggi35@gmail.com>
2021-11-16 22:23:58 +0900

    Add : Banner

---
+++ 03_upper_intermediate/01_04_MyAssets/MyAssets/MyAssets/AssetBanner.swift
@@ -0,0 +1,14 @@
+//
+//  AssetBanner.swift
+//  MyAssets
+//
+//  Created by Jeonggi Hong on 2021/11/16.
+//
+
+import UIKit
+
+struct AssetBanner {
+    let title: String
+    let description: String
+    let backgroundColor: UIColor
+}
