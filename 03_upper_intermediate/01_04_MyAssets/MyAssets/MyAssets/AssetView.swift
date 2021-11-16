6c3a00dfa829fbd6cad948598200ac2266d508d5
hongjeonggi <hongjeonggi35@gmail.com>
2021-11-16 21:10:47 +0900

    Add : Grid View

---
+++ 03_upper_intermediate/01_04_MyAssets/MyAssets/MyAssets/AssetView.swift
@@ -0,0 +1,29 @@
+//
+//  AssetView.swift
+//  MyAssets
+//
+//  Created by Jeonggi Hong on 2021/11/16.
+//
+
+import SwiftUI
+
+struct AssetView: View {
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                VStack(spacing: 30) {
+                    Spacer()
+                    AssetMenuGridView()
+                }
+            }
+            .background(Color.gray.opacity(0.2))
+            .navigationBarWithButtonStyle("내 자산")
+        }
+    }
+}
+
+struct AssetView_Previews: PreviewProvider {
+    static var previews: some View {
+        AssetView()
+    }
+}
