17ad588f8f1b85b1902b786bc5c0b6c69fb21713
hongjeonggi <hongjeonggi35@gmail.com>
2021-11-17 14:26:41 +0900

    Add : AppStore project and basic setting

---
+++ 03_upper_intermediate/02_01_AppStore/AppStore/AppStore/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  AppStore
+//
+//  Created by Jeonggi Hong on 2021/11/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+        
+    }
+
+
+}
+
