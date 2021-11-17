efd7990241badbe07eb5c8824ba3f918e79f0122
hongjeonggi <hongjeonggi35@gmail.com>
2021-11-17 16:21:33 +0900

    Add scrollView, contentView, stackView and test

--- 03_upper_intermediate/02_01_AppStore/AppStore/AppStore/Scenes/App/AppViewController.swift
+++ 03_upper_intermediate/02_01_AppStore/AppStore/AppStore/Scenes/App/AppViewController.swift
@@ -10,10 +10,41 @@ import UIKit
 
 final class AppViewController: UIViewController {
     
+    private let scrollView = UIScrollView()
+    private let contentView = UIView()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 0.0
+        
+        // 필요한것 임의로 지정
+        let featureSectionView = UIView()
+        let rankingFeatureSectionView = UIView()
+        let exchangeCodeButtonView = UIView()
+        
+        featureSectionView.backgroundColor = .red
+        rankingFeatureSectionView.backgroundColor = .blue
+        exchangeCodeButtonView.backgroundColor = .yellow
+        
+        [
+            featureSectionView,
+            rankingFeatureSectionView,
+            exchangeCodeButtonView
+        ].forEach {
+            $0.snp.makeConstraints { $0.height.equalTo(500.0) }
+            stackView.addArrangedSubview($0)
+        }
+        
+        return stackView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupNavigationController()
+        setupLayout()
     }
 }
 
@@ -23,4 +54,28 @@ private extension AppViewController {
         navigationItem.largeTitleDisplayMode = .always
         navigationController?.navigationBar.prefersLargeTitles = true
     }
+    
+    func setupLayout() {
+        view.addSubview(scrollView)
+        scrollView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            $0.bottom.equalToSuperview()
+            $0.leading.equalToSuperview()
+            $0.trailing.equalToSuperview()
+        }
+        
+        scrollView.addSubview(contentView)
+        contentView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+            $0.width.equalToSuperview() // 세로스크롤 가로 고정
+        }
+        
+        contentView.addSubview(stackView)
+        stackView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+    }
 }
+
+
