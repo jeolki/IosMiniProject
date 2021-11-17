b1a33393425cec43b56962dbc3e3231c8294393d
hongjeonggi <hongjeonggi35@gmail.com>
2021-11-17 15:47:46 +0900

    Implement Today Tab View controller

--- 03_upper_intermediate/02_01_AppStore/AppStore/AppStore/Scenes/Today/TodayViewController.swift
+++ 03_upper_intermediate/02_01_AppStore/AppStore/AppStore/Scenes/Today/TodayViewController.swift
@@ -20,7 +20,10 @@ final class TodayViewController: UIViewController {
         
         collectionView.backgroundColor = .systemBackground
         collectionView.register(TodayCollectionViewCell.self, forCellWithReuseIdentifier: "todayCell")
-        
+        collectionView.register(
+            TodayCollectionHeaderView.self,
+            forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+            withReuseIdentifier: "TodayCollectionHeaderView")
         return collectionView
     }()
     
@@ -47,6 +50,23 @@ extension TodayViewController: UICollectionViewDataSource {
         5
     }
     
+    // Header, Footer
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        
+        // header
+        guard kind == UICollectionView.elementKindSectionHeader,
+              let header = collectionView.dequeueReusableSupplementaryView(
+                ofKind: kind,
+                withReuseIdentifier: "TodayCollectionHeaderView",
+                for: indexPath
+              ) as? TodayCollectionHeaderView
+        else { return UICollectionReusableView() }
+        
+        header.setupViews()
+        
+        return header
+    }
+    
 }
 
 extension TodayViewController: UICollectionViewDelegateFlowLayout {
@@ -56,4 +76,13 @@ extension TodayViewController: UICollectionViewDelegateFlowLayout {
         return CGSize(width: width, height: width)
     }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        CGSize(width: collectionView.frame.width - 32.0, height: 100.0)
+    }
+    
+    // header 와 section의 사이 간격
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        let value: CGFloat = 16.0
+        return UIEdgeInsets(top: value, left: value, bottom: value, right: value)
+    }
 }
