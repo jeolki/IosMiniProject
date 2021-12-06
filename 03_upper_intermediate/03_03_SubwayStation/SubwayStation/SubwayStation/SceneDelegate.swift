b031748be1b596b47fcdea7cb4cb7e11fd4a5f4a
hongjeonggi <hongjeonggi35@gmail.com>
2021-12-06 20:36:04 +0900

    Add SubwayStation project and setting SnapKit, Alamofire and delete main.storyboard

---
+++ 03_upper_intermediate/03_03_SubwayStation/SubwayStation/SubwayStation/SceneDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  SceneDelegate.swift
+//  SubwayStation
+//
+//  Created by Jeonggi Hong on 2021/12/06.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        
+    }
+
+
+}
+
