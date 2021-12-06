e77583d2194da7d9bcee5407cc912bcfa8f95bac
hongjeonggi <hongjeonggi35@gmail.com>
2021-12-06 21:06:41 +0900

    Implement Search UI

---
+++ 03_upper_intermediate/03_01_SubwayStation/SubwayStation/SubwayStation/StationSearchViewController.swift
@@ -0,0 +1,73 @@
+//
+//  StationSearchViewController.swift
+//  SubwayStation
+//
+//  Created by Jeonggi Hong on 2021/12/06.
+//
+
+import SnapKit
+import UIKit
+
+class StationSearchViewController: UIViewController {
+    
+    private var numberOfCell: Int = 0
+
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.dataSource = self
+        tableView.isHidden = true
+        return tableView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        setNavigationItems()
+        setTableViewLayout()
+    }
+    
+    
+    private func setNavigationItems() {
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "지하철 도착 정보"
+        
+        let searchController = UISearchController()
+        searchController.searchBar.placeholder = "지하철역을 입력해주세요."
+        searchController.obscuresBackgroundDuringPresentation = false // TODO
+        searchController.searchBar.delegate = self // 위임
+        
+        navigationItem.searchController = searchController
+    }
+    
+    private func setTableViewLayout() {
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints { $0.edges.equalToSuperview() }
+    }
+
+}
+
+extension StationSearchViewController: UISearchBarDelegate {
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        numberOfCell = 10
+        tableView.isHidden = false
+    }
+    
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        numberOfCell = 0
+        tableView.isHidden = true
+    }
+}
+
+extension StationSearchViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return numberOfCell
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "\(indexPath.item)"
+        return cell
+    }
+    
+}
