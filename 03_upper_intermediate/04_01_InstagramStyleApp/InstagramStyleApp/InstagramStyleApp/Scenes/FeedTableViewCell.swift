c5a523e12541723db41cb3b2a82f855a606f9e34
hongjeonggi <hongjeonggi35@gmail.com>
2022-01-04 16:06:56 +0900

    Add FeedTableViewCell frame

---
+++ 03_upper_intermediate/04_01_InstagramStyleApp/InstagramStyleApp/InstagramStyleApp/Scenes/FeedTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  FeedTableViewCell.swift
+//  InstagramStyleApp
+//
+//  Created by Jeonggi Hong on 2022/01/04.
+//
+
+import SnapKit
+import UIKit
+
+final class FeedTableViewCell: UITableViewCell {
+    
+}
--- 03_upper_intermediate/04_01_InstagramStyleApp/InstagramStyleApp/InstagramStyleApp/Scenes/FeedViewController.swift
+++ 03_upper_intermediate/04_01_InstagramStyleApp/InstagramStyleApp/InstagramStyleApp/Scenes/FeedViewController.swift
@@ -10,15 +10,42 @@ import UIKit
 
 class FeedViewController: UIViewController {
 
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero)
+        tableView.backgroundColor = .systemBackground
+        tableView.separatorStyle = .none
+        tableView.dataSource = self
+        
+        // Cell 설정
+        tableView.register(FeedTableViewCell.self, forCellReuseIdentifier: "FeedTableViewCell")
+        
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     
         setupNavigationBar()
+        setupTableView()
     }
 
 }
 
+extension FeedViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FeedTableViewCell", for:  indexPath) as? FeedTableViewCell
+        cell?.selectionStyle = .none
+        
+        return cell ?? UITableViewCell()
+    }
+}
+
 private extension FeedViewController {
+    
     func setupNavigationBar() {
         navigationItem.title = "Instagram"
         
@@ -30,4 +57,10 @@ private extension FeedViewController {
         )
         navigationItem.rightBarButtonItem = uploadButton
     }
+    
+    func setupTableView() {
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints{ $0.edges.equalToSuperview() }
+    }
+    
 }
