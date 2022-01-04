4ca9f6595154dd1fad4775581a2b851e5c437d7b
hongjeonggi <hongjeonggi35@gmail.com>
2022-01-04 15:38:41 +0900

    Add InstagramStyleApp project and description

---
+++ 03_upper_intermediate/04_01_InstagramStyleApp/InstagramStyleApp/InstagramStyleApp/SceneDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  SceneDelegate.swift
+//  InstagramStyleApp
+//
+//  Created by Jeonggi Hong on 2022/01/04.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+
+
+}
+
---
+++ 03_upper_intermediate/04_01_InstagramStyleApp/README.md
@@ -0,0 +1,1 @@
+# ios_mini_project
