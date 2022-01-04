49d5430fab12d5eaa6a82b53d212b272309e490a
hongjeonggi <hongjeonggi35@gmail.com>
2022-01-04 15:45:11 +0900

    Update temporary TabBarController setting

---
+++ 03_upper_intermediate/04_01_InstagramStyleApp/InstagramStyleApp/InstagramStyleApp/Scenes/TabBarController.swift
@@ -0,0 +1,32 @@
+//
+//  TabBarController.swift
+//  InstagramStyleApp
+//
+//  Created by Jeonggi Hong on 2022/01/04.
+//
+
+import UIKit
+
+final class TabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // TabBar 설정
+        let feedViewController = UIViewController() // 임의
+        feedViewController.tabBarItem = UITabBarItem(
+            title: nil,
+            image: UIImage(systemName: "house"),
+            selectedImage: UIImage(systemName: "house.fill")
+        )
+        
+        let profileViewController = UIViewController() // 임의
+        profileViewController.tabBarItem = UITabBarItem(
+            title: nil,
+            image: UIImage(systemName: "person"),
+            selectedImage: UIImage(systemName: "person.fill")
+        )
+        
+        // 2가지 등록
+        viewControllers = [feedViewController, profileViewController]
+    }
+}
